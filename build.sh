4f737241148a0a7cdcf5df984dc0cb4ea93d9deb
Ben Taylor <ben.taylor@sanger.ac.uk>
2017-12-13 14:54:36 +0000

    Build cores during build step

--- build.sh
+++ build.sh
@@ -6,10 +6,20 @@ for d in $(find ./schemes -mindepth 1 -maxdepth 1 -type d); do
   scheme_dir=$(cd $d && pwd)
   scheme=$(basename $d);
   if [[ -d databases/$scheme ]]; then
-    echo "Skipping $scheme";
-    continue;
+    echo "Skipping BlastDb for $scheme";
+  else
+    echo "Building $scheme"
+    mkdir -p databases/$scheme
+    awk -F ',' '{printf ">%s\n%s\n", $2, $4}' ${scheme_dir}/core.csv | tee databases/${scheme}/core.fa | makeblastdb -title core -in - -dbtype nucl -out databases/${scheme}/core.db  
   fi
-  echo "Building $scheme"
-  mkdir -p databases/$scheme
-  awk -F ',' '{printf ">%s\n%s\n", $2, $4}' ${scheme_dir}/core.csv | tee databases/${scheme}/core.fa | makeblastdb -title core -in - -dbtype nucl -out databases/${scheme}/core.db  
+  mkdir -p databases/${scheme}/references;
+  for ref in $(ls $scheme_dir/fastas/*.fasta); do
+    ref_name=$(basename $ref '.fasta');
+    if [[ -f databases/${scheme}/references/${ref_name}.core.json ]]; then
+      echo "Skipping core for $ref";
+    else
+      echo "Building core for $ref";
+      cat $ref | DEBUG='*' WGSA_ORGANISM_TAXID=$scheme node index.js > databases/${scheme}/references/${ref_name}.core.json
+    fi
+  done
 done
