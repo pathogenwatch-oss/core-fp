73bcb73fa88d46708ee269771acff81ed8819eb4
Ben Taylor <ben.taylor@sanger.ac.uk>
2017-12-15 16:03:24 +0000

    Speed up Docker build

--- build.sh
+++ build.sh
@@ -12,14 +12,15 @@ for d in $(find ./schemes -mindepth 1 -maxdepth 1 -type d); do
     mkdir -p databases/$scheme
     awk -F ',' '{printf ">%s\n%s\n", $2, $4}' ${scheme_dir}/core.csv | tee databases/${scheme}/core.fa | makeblastdb -title core -in - -dbtype nucl -out databases/${scheme}/core.db  
   fi
-  mkdir -p databases/${scheme}/references;
-  for ref in $(ls $scheme_dir/fastas/*.fasta); do
-    ref_name=$(basename $ref '.fasta');
-    if [[ -f databases/${scheme}/references/${ref_name}.core.json ]]; then
-      echo "Skipping core for $ref";
-    else
-      echo "Building core for $ref";
-      DEBUG='*' WGSA_ORGANISM_TAXID=$scheme node index.js $ref > databases/${scheme}/references/${ref_name}.core.json
-    fi
-  done
+  # This isn't used yet and slows down builds
+  # mkdir -p databases/${scheme}/references;
+  # for ref in $(ls $scheme_dir/fastas/*.fasta); do
+  #   ref_name=$(basename $ref '.fasta');
+  #   if [[ -f databases/${scheme}/references/${ref_name}.core.json ]]; then
+  #     echo "Skipping core for $ref";
+  #   else
+  #     echo "Building core for $ref";
+  #     DEBUG='*' WGSA_ORGANISM_TAXID=$scheme node index.js $ref > databases/${scheme}/references/${ref_name}.core.json
+  #   fi
+  # done
 done
