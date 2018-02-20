690d3ab1f52dd517a4ac790fdc38efa36e97fa1e
CorinY <dr.yeats@gmail.com>
2018-02-20 12:27:59 +0000

    Minor edit.

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ nthreads=0
 if [ -z ${1+x} ]; then
   proc_count=`cat /proc/cpuinfo | awk '/^processor/{print $3}' | tail -1`;
   if [ "${proc_count}" -gt "1" ]; then
-    nthreads=`expr $proc_count - 1`; # Leave one core for other processes
+    nthreads=`expr ${proc_count} - 1`; # Leave one core for other processes
   else
     nthreads=1;
   fi
@@ -22,23 +22,3 @@ else
 fi
 
 find ./schemes -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -P ${nthreads} -I scheme ./build-library.sh scheme
-
-#for d in $(find ./schemes -mindepth 1 -maxdepth 1 -type d); do
-#  scheme_dir=$(cd $d && pwd)
-#  scheme=$(basename $d);
-#  if [[ -d ${CORE_DB_ROOT}/$scheme ]]; then
-#    echo "Skipping BlastDb for $scheme";
-#  else
-#    echo "Building $scheme"
-#    mkdir -p ${CORE_DB_ROOT}/$scheme
-#    awk -F ',' '{printf ">%s\n%s\n", $2, $4}' ${scheme_dir}/core.csv | tee ${CORE_DB_ROOT}/${scheme}/core.fa | makeblastdb -title core -in - -dbtype nucl -out ${CORE_DB_ROOT}/${scheme}/core.db
-#  fi
-#  scheme_fp="${CORE_DB_ROOT}/${scheme}/fp.json"
-#  if [[ -f $scheme_fp ]]; then
-#    echo "Skipping FP for $scheme";
-#  else
-#    echo "Building FP for $scheme"
-#    mkdir -p ${CORE_DB_ROOT}/$scheme
-#    WGSA_ORGANISM_TAXID=$scheme node index.js build ${scheme_dir}/fastas/*.fasta
-#  fi
-#done
