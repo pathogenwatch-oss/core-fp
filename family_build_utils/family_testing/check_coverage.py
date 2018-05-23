4ba74538e81a663906ff4f97504215e0b9a02a6e
Corin Yeats <corin.yeats@gmail.com>
2018-05-23 17:42:51 +0100

    Add a coverage checking script.

---
+++ family_build_utils/family_testing/check_coverage.py
@@ -0,0 +1,34 @@
+import sys
+import jsonpickle
+from collections import defaultdict
+from pathlib import Path
+
+json_dir = Path(sys.argv[1])
+
+counts = defaultdict(lambda: 0)
+fragments = defaultdict(lambda: 0)
+
+for file in json_dir.iterdir():
+
+    if not str(file).endswith('json'):
+        continue
+
+    matches = jsonpickle.decode(file.read_text())['hits']
+
+    seen = set()
+
+    for match in matches:
+        hit_id = match['hitId']
+        if hit_id not in seen:
+            seen.add(hit_id)
+            counts[hit_id] += 1
+            if '*' in hit_id:
+                fragments[hit_id] += 1
+
+with open('family_counts.csv', 'w') as fc:
+    for family_id in counts:
+        print(family_id, counts[family_id], file=fc)
+
+with open('family_fragment_counts.csv', 'w') as ffc:
+    for family_id in fragments:
+        print(family_id, fragments[family_id], file=ffc)
